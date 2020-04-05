67eeb2bd8d47065ec84b8a2a0df7d352dc518cd1
dogrodnik <d.ogrodnik96@gmail.com>
2020-04-05 14:14:42 +0200

    Add simple style

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { useFetch } from "../../custom-hooks";
 
 export const PokemonCard = ({ name, url }) => {
+  const [imgPath, setImgPath] = useState("");
+  const { response, error } = useFetch(url);
+
+  useEffect(() => {
+    if (Boolean(response)) setImgPath(response.sprites.front_default);
+  }, [response, name]);
+
   return (
     <div className="pokemon-card">
-      <p>{name}</p>
-      <p>{url}</p>
+      <p>{name.charAt(0).toUpperCase() + name.substring(1)}</p>
+      <img src={imgPath} alt="pokemn" />
     </div>
   );
 };
