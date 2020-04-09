7bd29938338f456c19c7355b82f7687b4f63012a
dogrodnik <d.ogrodnik96@gmail.com>
2020-04-09 15:37:29 +0200

    Updates

---
+++ precache-manifest.530d87b6b2ca219ea429d085105207bc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e5c3c9c33f45f7d66ea37299c6cc99",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "4e5ed6d9a1e323136e86",
+    "url": "/pokedex/static/css/main.45022dea.chunk.css"
+  },
+  {
+    "revision": "6307fdefdcea7ebc78af",
+    "url": "/pokedex/static/js/2.216e34d6.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/pokedex/static/js/2.216e34d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e5ed6d9a1e323136e86",
+    "url": "/pokedex/static/js/main.3558f3ee.chunk.js"
+  },
+  {
+    "revision": "23e4c6896b4e85d076c4",
+    "url": "/pokedex/static/js/runtime-main.3f186e6a.js"
+  },
+  {
+    "revision": "45fe1d494234d630e4888ecc3e0284f3",
+    "url": "/pokedex/static/media/attack.45fe1d49.png"
+  },
+  {
+    "revision": "4f3cd58bc3a7e6d6281f729f42ad2eff",
+    "url": "/pokedex/static/media/defense.4f3cd58b.png"
+  },
+  {
+    "revision": "5289cc89afa7749e6e2bff8546474eb8",
+    "url": "/pokedex/static/media/health.5289cc89.png"
+  },
+  {
+    "revision": "f5fbd090ff370de9cca0d01b4f202f28",
+    "url": "/pokedex/static/media/pokeball-black.f5fbd090.png"
+  },
+  {
+    "revision": "ba197c3b76e9b06f9ed822895e97e756",
+    "url": "/pokedex/static/media/pokeball.ba197c3b.png"
+  },
+  {
+    "revision": "6f1fd44625c52ccd693ca4e39b23cc9f",
+    "url": "/pokedex/static/media/pokedex_logo.6f1fd446.png"
+  }
+]);
