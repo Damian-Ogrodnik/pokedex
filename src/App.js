b511e95d7b4d05a24217df2f0cfe040e43bb5128
dogrodnik <d.ogrodnik96@gmail.com>
2020-04-11 20:38:30 +0200

    Welcome component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 
 import { Pokedex } from "./components/Pokedex";
+import { Welcome } from "./components/Welcome";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <Welcome />
         <Pokedex />
       </div>
     </Provider>
---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+import pokeballImg from "../../assets/pokeball.png";
+
+export const Welcome = () => {
+  const [isOpen, setOpen] = useState(true);
+  const [isOut, setOut] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setOut("out");
+    }, 3500);
+    setTimeout(() => {
+      setOpen(false);
+    }, 5500);
+  }, []);
+
+  return (
+    <>
+      <div className="curtain curtain__left"></div>
+      {isOpen && (
+        <p className={`welcome ${isOut}`}>
+          <p className={`welcome__header--up ${isOut}`}>Pokedex</p>
+          <img
+            className={`welcome__img ${isOut}`}
+            src={pokeballImg}
+            alt="Pokeball"
+          />
+          <p className={`welcome__header--down ${isOut}`}>Welcome</p>
+        </p>
+      )}
+      <div className="curtain curtain__right"></div>
+    </>
+  );
+};
